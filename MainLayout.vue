985621b6a7cd40b4603b081c537269c48d67261c
Cru1zik <93572342+Cru1zik@users.noreply.github.com>
2022-01-28 19:16:47 +0300

    Add files via upload

---
+++ MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Визуализация ключевых моментов в текстах об компьютерных играх
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Ссылки
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Главная',
+    caption: 'localhost:8080/#/',
+    icon: 'school',
+    link: 'http://localhost:8080/#/'
+  },
+  {
+    title: 'Ссылка на исходник в GitHub',
+    caption: 'https://github.com/Cru1zik',
+    icon: 'code',
+    link: 'https://github.com/Cru1zik'
+  },
+  {
+    title: '-',
+    caption: '',
+    icon: 'chat',
+    link: '-'
+  },
+ 
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
